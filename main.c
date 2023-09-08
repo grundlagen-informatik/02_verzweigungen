178aaf766240ced61f7044860840da4cb408ed04
isabelge <isabel.geissmann@unibas.ch>
2023-09-08 15:38:16 +0200

    add solution

--- main.c
+++ main.c
@@ -24,9 +24,63 @@ int main() {
 }
 
 void bmi() {
-   // Hier muss der Code für die BMI-Aufgabe geschrieben werden.
+    float groesse;
+    float gewicht;
+    float bmi;
+
+    printf("Bitte geben Sie das Gewicht in kg ein: ");
+    scanf_s("%f", &gewicht);
+    printf("Bitte geben Sie die Koerpergroesse in m ein: ");
+    scanf_s("%f", &groesse);
+
+    bmi = gewicht / (groesse * groesse);
+
+    if (bmi < 18.5) {
+        printf("Der BMI ist %f \n", bmi);
+        printf("Untergewicht");
+    } else if (bmi >= 18.5 && bmi < 25.0) {
+        printf("Der BMI ist %f \n", bmi);
+        printf("Normalgewicht");
+    } else {
+        printf("Der BMI ist %f \n", bmi);
+        printf("Uebergewicht");
+    }
 }
 
 void automat() {
-    // Hier muss der Code für die Getränke-Automat-Aufgabe geschrieben werden.
+
+    int getraenk;
+    float geld;
+
+    printf("Waelen Sie ihr Getraenk aus: \n 1) Wasser (0,50 Euro)\n 2) Limonade (1,00 Euro) \n 3) Bier (2,00 Euro)\n\n Geben sie 1, 2 oder 3 ein: ");
+
+    scanf_s("%d", &getraenk);
+
+    if (getraenk == 1) {
+        printf("Bitte werfen Sie 0.50 Euro ein: ");
+        scanf_s("%f", &geld);
+        if (geld == 0.5) {
+            printf("Vielen Dank, bitte entnehmen Sie das Getraenk.");
+        } else {
+            printf("Bitte werfen Sie den Betrag passend ein.");
+        }
+    } else if (getraenk == 2) {
+        printf("Bitte werfen Sie 1.0 Euro ein: ");
+        scanf_s("%f", &geld);
+        if (geld == 1.0) {
+            printf("Vielen Dank, bitte entnehmen Sie das Getraenk.");
+        } else {
+            printf("Bitte werfen Sie den Betrag passend ein.");
+        }
+    } else if (getraenk == 3) {
+        printf("Bitte werfen Sie 2.00 Euro ein: ");
+        scanf_s("%f", &geld);
+        if (geld == 2.0) {
+            printf("Vielen Dank, bitte entnehmen Sie das Getraenk.");
+        } else {
+            printf("Bitte werfen Sie den Betrag passend ein.");
+        }
+    } else {
+        printf("Bitte wählen Sie eine gültige Nummer.");
+    }
 }
